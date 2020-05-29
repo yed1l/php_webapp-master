d0b694c34df902ca8ca99626c0a233df41c4d4c0
Yedil Zharkynuly <yed1l>
2020-05-29 14:02:40 +0600

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,18 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Email: info@iitu.kz
+                </div> <div class="col-md-12">
+                    Call: +7(771) 701 44 66
+                </div>
+                <div class="col-md-12">
+                    Addres: г. Алматы, ул. Манаса 34/1
+                </div>
+                <div class="col-md-12">
+                    &copy; 2020 IITU Online Library Management System |<a href="https://github.com/yed1l" target="_blank"  > Done by : Yedil</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
